cd7648a3a471130f567f9a5de42ef89326981e84
unknown <sniikz10000@gmail.com>
2023-09-19 18:42:17 -0500

    new

---
+++ api/index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+dotenv.config() 
+const app = express();
+const connect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Conectados a MONGODB");
+      } catch (error) {
+        //handleError(error);
+        throw error;
+      }
+}
+mongoose.connection.on('connected',()=>{
+    console.log("MongoDB Conectado!!!");
+})
+mongoose.connection.on('disconnected',()=>{
+    console.log("MongoDB Desconectado!!!");
+})
+
+app.use(express.json());
+
+//middLewares
+app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/hotels",hotelsRoute);
+app.use("/api/rooms",roomsRoute);
+
+app.listen (8800, ()=>{
+    connect()
+    console.log('Conectado al backend!!!jjj')
+} )
+
+app.get("/reservas",(req, res)=>{
+    res.send(200,"Hola todo ok");
+})
