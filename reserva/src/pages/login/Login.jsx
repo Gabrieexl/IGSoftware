a32928c9a3d004203192b12cad0bfe9eb8c8a5e8
unknown <sniikz10000@gmail.com>
2023-10-31 22:37:39 -0500

    [ADD] Video 10-17 realizados

--- reserva/src/pages/login/Login.jsx
+++ reserva/src/pages/login/Login.jsx
@@ -1,44 +1,87 @@
-import axios from "axios";
-import { AuthContex } from "../../context/AuthContext";
-import "./login.css"
-import { useAsyncError } from "react-router-dom";
 import { useContext, useState } from "react";
+import "./login.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import toast, {Toaster} from "react-hot-toast";
+import { AuthContex } from "../../context/AuthContext";
 
 const Login = () => {
     const [credencials,setCredentians] = useState({
         username:undefined,
         password:undefined,
-
     });
-    const {user,loading, error, dispatch} = useContext(AuthContex);
-    const handleChange = () =>{
-        setCredentians((prev)=>({...prev,[e.target.id]:e.target.value}));       
-    }
 
-    const login = async (e) =>{
+    const {user, loading, error, dispatch} = useContext(AuthContex)
+    const navigate = useNavigate()
+
+    const handleChange = (e) => {
+        setCredentians((prev) =>({...prev,[e.target.id]:e.target.value}));
+    }
+    const login = async (e) => {
         e.preventDefault();
+        
         dispatch({type:"LOGIN_START"});
         try{
-            const res = await axios.post("/auth/login", credencials);
+            toast.loading("Espere..")
+            const res = await axios.post("/auth/login",credencials);
             dispatch({type:"LOGIN_SUCCESS",payload:res.data});
+            toast.dismiss()
+            toast.success("Bienvenid@ al sistema",{
+                duration: 2000,
+                position: 'top-right'
+            })
+            toast.loading("Cargando Propiedades..")
+            
+            setTimeout(()=>{
+                toast.dismiss()
+                navigate("/")
+            },1500)
         }catch(err){
-            dispatch({type:"LOGIN_FAILURE",payload:err.response.data});
-
+            toast.dismiss()
+            toast.error(`Error de acceso: ${err.response.data.message}`,
+            {
+                duration: 3000,
+                position: 'top-right'
+            })
+            dispatch({type:"LOGIN_FAILURE",payload:err.response.data})
         }
     }
+
+    console.log(user)
+
     return (
-        <div className="login">
-            <div className="lContainer">
-                <input type="text" placeholder="Nombre de usuario" id="username" className="lInput"
-                onChange={handleChange}
-                />
-                <input type="password" placeholder="Contraseña" id="password" className="lInput"
-                onChange={handleChange}
-                />
-                <button className="lButton" onClick={login}>Ingresar</button>
-                {error && <span>{error.message}</span>}
+        <div className="container-login">
+            <div className="main">
+                <input type="checkbox" id="label" arias-hidden="true"/>
+            <div className="login">
+                <label for="label">Iniciar Sesion</label>
+                    <input type="text" placeholder="Nombre de usuario" id="username" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <input type="password" placeholder="Contraseña" id="password" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <button className="lButton" onClick={login}>Ingresar</button>
+                    {error && <span>{error.message}</span>}
+            </div>
+            <div className="register">
+            <label for="label">Registro</label>
+                    
+                    <input type="text" placeholder="Nombre de usuario" id="username" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <input type="password" placeholder="Contraseña" id="password" className="lInput" 
+                    onChange={handleChange}
+                    />
+                    <button className="lButton" onClick={login}>registrar</button>
+                    {error && <span>{error.message}</span>}
+                
             </div>
         </div>
+        <Toaster/>
+        </div>
+    
     )
 }
+
 export default Login
